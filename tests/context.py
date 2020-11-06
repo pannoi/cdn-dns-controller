707f608e23c23c5dcd0c15b09d8fad6f15c454ea
pannoi <anton.poplavnoi@outlook.com>
2020-11-06 10:46:12 +0200

    test context: add all modules

--- tests/context.py
+++ tests/context.py
@@ -8,3 +8,6 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 from src.environment import Environment
 from main import app 
 from src.route53 import Route53
+from src.cloudfront import CloudFront
+from src.acm import ACM
+from src.helpers import Helper
